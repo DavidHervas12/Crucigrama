0a7fabaa0cc58f5c2fdf2a8dc3e537f62c39a642
DavidHervas12 <121380511+DavidHervas12@users.noreply.github.com>
2023-10-31 16:49:31 +0100

    Refactoring Screen2

--- hola.js
+++ hola.js
@@ -1,3 +1,12 @@
-import getData from "./services/services";
-
-console.log(getData("https://api.thecatapi.com/v1/images/search?size=full"));
+const board = () => {
+    let array1 = [];
+    for (let i = 0; i < 12; i++) {
+      let array2 = [];
+      for (let j = 0; j < 10; j++) {
+        array2.push(j);
+      }
+      array1.push(array2);
+    }
+    return array1;
+  };
+console.log(board());
